6d3a1cf1df4d021399f67adec6e4dea167dc2d5d
Nazape-star <m.nazape@gmail.com>
2026-06-19 19:52:18 -0300

    Atualiza nome da Travessia Essencial

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const phoneNumber = "5591988369983";
 const message =
-  "Olá, Nazaré! Tenho interesse em participar da Travessia Essencial — O Chamado de Volta para Mim. Gostaria de receber mais informações.";
+  "Olá, Nazaré! Tenho interesse em participar da Travessia Essencial — O caminho de volta para mim. Gostaria de receber mais informações.";
 
 const whatsappLink = document.querySelector("#whatsapp-link");
 
